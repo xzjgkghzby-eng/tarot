c4fe18c01583158bd9ce783210c4bddbfbcbaa4e
Xurune <xzjgkghzby@privaterelay.appleid.com>
2026-03-16 16:50:54 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,18 @@
-const CACHE = 'tarot-v2';
-const FILES = [
-    '/tarot-compteur/',
-    '/tarot-compteur/index.html',
-    '/tarot-compteur/manifest.json'
-];
+const CACHE = 'tarot-v3';
 
 self.addEventListener('install', e => {
     self.skipWaiting();
-    e.waitUntil(
-        caches.open(CACHE).then(cache => cache.addAll(FILES))
-    );
 });
 
 self.addEventListener('activate', e => {
     e.waitUntil(
         caches.keys().then(keys =>
-            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+            Promise.all(keys.map(k => caches.delete(k)))
         )
     );
     self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(r => r || fetch(e.request))
-    );
+    e.respondWith(fetch(e.request));
 });
