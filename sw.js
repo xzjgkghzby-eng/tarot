cabf62eb688086913a9d27b8d6da34f98a4787a7
Xurune <xzjgkghzby@privaterelay.appleid.com>
2026-03-16 16:31:45 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'tarot-v1';
+const FILES = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(FILES))
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
+});
