4440ed70f6e6a297c0db1ae43e9808ba392fd68c
Xurune <xzjgkghzby@privaterelay.appleid.com>
2026-03-17 16:09:13 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,25 @@
-const CACHE = 'tarot-v3';
+const CACHE = 'jeux-cartes-v1';
+const FILES = [
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', e => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-    e.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(keys.map(k => caches.delete(k)))
-        )
-    );
-    self.clients.claim();
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(fetch(e.request));
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
 });
